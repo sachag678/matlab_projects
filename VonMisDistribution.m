a86702846b4f1a0fd08ee73a0b2347c58e346621
sachag678 <sacha.gunaratne@gmail.com>
2017-09-06 15:09:23 -0400

    Updated x with input

--- VonMisDistribution.m
+++ VonMisDistribution.m
@@ -2,7 +2,7 @@ function [ prob ] = VonMisDistribution( input,mu,k )
 %Calculates the probabilty of input given mu and k using the 
 %von mis distribution
 
-prob = exp(k*cos(x-mu))/(2*pi*besseli(0,k));
+prob = exp(k*cos(input-mu))/(2*pi*besseli(0,k));
 
 end
 
