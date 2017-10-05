49ec031239ee647bb6044e6f3753922e9ee68c5d
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2017-10-05 12:46:38 -0400

    Test the vonMises EM algorithm and compare it to the ML estimate. Resulting in similar probabilities.

--- twonode_dsc2cts.m
+++ twonode_dsc2cts.m
@@ -13,13 +13,17 @@ bnet.CPD{2} = vonMises_CPD(bnet, 2); %vonMises
 bnet.CPD{1} = tabular_CPD(bnet, 1); %tabular
 
 %get data
-data = [2 2.3; 1 1.6; 1 1.3; 2 2.5];
+data = [2 0.5*pi; 1 1.8*pi; 1 1.3*pi; 2 0.2*pi; 2 0.1*pi; 1 1.9*pi];
 ncases = size(data, 1);			% number of data points
 cases = cell(n,ncases);		% create an empty table to store the data to be given to the learning algorithm
 cases(:,:) = num2cell(data(:,:)');	% copy the data
 
 %learn
+%learn EM
+%engine = jtree_inf_engine(bnet);
 %[bnet2, ~, engine] = learn_params_em(engine, cases);
+
+%learn ML
 bnet2 = learn_params(bnet, cases);
 engine = jtree_inf_engine(bnet2);
 
